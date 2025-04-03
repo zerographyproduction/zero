aa7a1b3559f23f5e202f0757f5a1b7889c1e70e5
zerographyproduction <zerographyproduction@gmail.com>
2025-04-03 23:08:42 +0530

    Update page.jsx

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -77,7 +77,7 @@ const teamMembers = [
       'Gautam, our specialist in architectural photography, brings 8 years of experience capturing the essence of design and structure. With a keen eye for detail and a profound understanding of composition, he transforms spaces into visually compelling narratives.',
   },
   {
-    image: Arindam Roy,
+    image: Arindam,
     name: 'Arindam Roy',
     role: 'Sr. Creative Director, Writer',
     description:
